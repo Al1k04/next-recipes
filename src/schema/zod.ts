95e06a3cb416bbf1459833d74dde69d662edd7ce
Al1k04 <alikprog04@gmail.com>
2026-05-11 16:25:36 +0400

    fix: zod v4 required_error -> error

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -1,12 +1,12 @@
-import z, { object, string } from "zod"; // ← убрали number из импорта
+import z, { object, string } from "zod";
 
 export const signInSchema = object({
-  email: string({ required_error: "Email is required" })
+  email: string({ error: "Email is required" })
     .min(1, "Email is required")
     .email("Invalid email"),
-  password: string({ required_error: "Password is required" })
+  password: string({ error: "Password is required" })
     .min(1, "Password is required")
-    .min(6, "Password must be more than 8 characters")
+    .min(6, "Password must be more than 6 characters")
     .max(32, "Password must be less than 32 characters"),
 });
 
@@ -22,8 +22,8 @@ export const ingredientSchema = object({
   ]),
   unit: z.enum(["GRAMS", "KILOGRAMS", "LITERS", "MILLILITER", "PIECES"]),
   pricePerUnit: z.coerce
-    .number({ invalid_type_error: "Цена должна быть числом" })
-    .min(0, "Цена должна быть положительной")
+    .number()
+    .min(0, "The price must be positive")
     .nullable()
     .optional(),
   description: z.string().optional(),
